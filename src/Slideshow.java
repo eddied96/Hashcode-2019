d39ddc2dc60149b23951e754ae44694a4d1e482e
Fergus-Ryan <fergusryan@live.ie>
2019-02-28 21:04:40 +0000

    stuff

--- src/Slideshow.java
+++ src/Slideshow.java
@@ -21,9 +21,16 @@ public class Slideshow {
 		
 		int numPhotos = Integer.parseInt(sc.nextLine());
 		ArrayList<List<String>> photos = new ArrayList<List<String>>();
+		ArrayList<List<String>> photos2 = new ArrayList<List<String>>();
 		int count = 0;
 		while(sc.hasNextLine()) {
-			photos.add(Arrays.asList(sc.nextLine().split(" ")));
+			ArrayList<String> line = new ArrayList<String>();
+			line.addAll(Arrays.asList(sc.nextLine().split(" ")));
+			line.remove(0);
+			line.remove(0);
+			Collections.sort(line);
+			photos.add(line);
+			photos2.add(line);
 			
 			count++;
 		}
@@ -31,19 +38,21 @@ public class Slideshow {
 		int slideCount = 0;
 		int[] slides = new int[numPhotos];
 		
-		slides[0] = 0;
+		Collections.shuffle(photos);
+		slides[0] = 1;
 		slideCount++;
 		List<String> currentPhoto = new ArrayList<String> ();
-		currentPhoto = photos.get(0);
-		photos.remove(0);
+		currentPhoto = photos.get(1);
+		photos.remove(1);
 		
 		int nextPhoto = 0;
 		int currentTag = 0;
-		for (int i=0; i<numPhotos;i++) {
+		boolean noMatch = false;
+		while(noMatch ==false) {
 			if(!photos.isEmpty()) {
 				if(currentTag < currentPhoto.size()) {
-					if(Collections.binarySearch(photos.get(nextPhoto), currentPhoto.get(currentTag)) != -1) {
-						slides[slideCount] = Integer.parseInt(photos.get(nextPhoto).get(1));
+					if(Collections.binarySearch(photos.get(nextPhoto), currentPhoto.get(currentTag)) > -1) {
+						slides[slideCount] = photos2.indexOf(photos.get(nextPhoto));
 						slideCount++;
 						currentPhoto = photos.get(nextPhoto);
 						photos.remove(nextPhoto);
@@ -55,8 +64,14 @@ public class Slideshow {
 				}
 				else {
 					currentTag = 0;
-					nextPhoto = (nextPhoto+1)%photos.size();
+					nextPhoto++;
+					if(nextPhoto >= photos.size()) {
+						noMatch =true;
+					}
 				}
+			}
+			else {
+				noMatch =true;
 			}
 		}
 		
@@ -67,6 +82,7 @@ public class Slideshow {
 				bw.write(Integer.toString(slides[i]) + "\n");
 				bw.flush();
 			}
+			System.out.println(slideCount);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
