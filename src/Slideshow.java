f5d9d4346dfecb55ebcc8b54f871e6159c0b0d6a
Fergus-Ryan <fergusryan@live.ie>
2019-02-28 19:45:33 +0000

    stuff

--- src/Slideshow.java
+++ src/Slideshow.java
@@ -31,26 +31,40 @@ public class Slideshow {
 		int slideCount = 0;
 		int[] slides = new int[numPhotos];
 		
-		slides[0] = 1;
+		slides[0] = 0;
 		slideCount++;
 		List<String> currentPhoto = new ArrayList<String> ();
 		currentPhoto = photos.get(0);
 		photos.remove(0);
 		
-		int numMatches = 0;
 		int nextPhoto = 0;
 		int currentTag = 0;
-		while (!photos.isEmpty()) {
-			if(Collections.binarySearch(photos.get(nextPhoto), currentPhoto.get(currentTag)) != -1) {
-				numMatches++;
+		for (int i=0; i<numPhotos;i++) {
+			if(!photos.isEmpty()) {
+				if(currentTag < currentPhoto.size()) {
+					if(Collections.binarySearch(photos.get(nextPhoto), currentPhoto.get(currentTag)) != -1) {
+						slides[slideCount] = Integer.parseInt(photos.get(nextPhoto).get(1));
+						slideCount++;
+						currentPhoto = photos.get(nextPhoto);
+						photos.remove(nextPhoto);
+						currentTag=0;
+						nextPhoto=0;
+					}else {
+						currentTag++;
+					}
+				}
+				else {
+					currentTag = 0;
+					nextPhoto = (nextPhoto+1)%photos.size();
+				}
 			}
 		}
 		
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\F\\Documents\\HashCode-2019\\submission.txt"));
-			bw.write(Integer.toString(numPhotos) + "\n");
-			for(int i=0;i<numPhotos;i++) {
-				bw.write(Integer.toString(i) + "\n");
+			bw.write(Integer.toString(slideCount) + "\n");
+			for(int i=0;i<slideCount;i++) {
+				bw.write(Integer.toString(slides[i]) + "\n");
 				bw.flush();
 			}
 		} catch (IOException e) {
